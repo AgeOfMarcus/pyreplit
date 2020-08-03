4b29e65b0f79a1df95eb2a3d078c34bce6a1eea0
Marcus Weinberger <marcus@marcusj.tech>
2020-08-03 23:26:55 +0000

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os
 
 setuptools.setup(name="pyreplit",
-    version="0.2.1",
+    version="0.2.2",
     description="Fork of sugarfi/pyrepl",
     long_description=open('README.md','r').read(),
     long_description_content_type="text/markdown",

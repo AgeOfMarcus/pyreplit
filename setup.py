177c814d51d3f79067d2bff0b3ae1768b68c6a6b
Marcus Weinberger <marcus@marcusj.tech>
2020-08-03 20:39:27 +0000

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 import setuptools
 import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setuptools.setup(name="pyreplit",
-    version="0.0.4",
+    version="0.0.5",
     description="Fork of sugarfi/pyrepl",
-    long_description=read('README.md'),
+    long_description=open('README.md','r').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/AgeOfMarcus/pyreplit",
     author="AgeOfMarcus",
     author_email="marcus@marcusweinberger.com",
     packages=setuptools.find_packages(),
     zip_safe=False,
-    install_requires=read('requirements.txt').split('\n'),
+    install_requires=open('requirements.txt','r').read().split('\n'),
 )
